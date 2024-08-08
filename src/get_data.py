4058348721cfc2ec207ffac3ad0c56d69245bcbb
Olexandr Savchuk <ppos19soo@gmail.com>
2024-08-08 15:26:29 +0300

    fix awaiting

--- src/get_data.py
+++ src/get_data.py
@@ -3,9 +3,9 @@ import asyncio
 from country_data_manager import CountryDataManager
 
 
-def main():
+async def main():
     cdm = CountryDataManager()
-    cdm.update_countries()
+    await cdm.update_countries()
 
 
 if __name__ == "__main__":
